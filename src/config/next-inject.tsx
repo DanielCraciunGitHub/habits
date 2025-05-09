24302f47269e34d990e97bb782261efb4197ee00
Daniel Craciun <danielcraciun111@yahoo.com>
2025-05-09 22:10:59 +0100

    init sidebar + jotai

--- src/config/next-inject.tsx
+++ src/config/next-inject.tsx
@@ -1,12 +1,13 @@
 // ? Change these details
-export const projectName = "PROJECTNAME";
+export const projectName = "Habit Tracker";
 
+// TODO: Change these details
 export const siteConfig = {
   email: `danielcracbusiness@gmail.com`,
   url:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://next15template.vercel.app",
+      : "https://habit-tracker-brown.vercel.app",
   footerText:
     `© ${new Date().getFullYear()} ${projectName}. All Rights Reserved` as const satisfies string,
 };
